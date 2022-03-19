b921e23606bb2bb18743e770b25da9b7c38ec53f
Amir Tawfik <amirtawfik@Amirs-MacBook-Pro.local>
2022-03-19 14:05:22 -0400

    dom traversing unit with prompts

--- dom-traversing/script.js
+++ dom-traversing/script.js
@@ -4,7 +4,10 @@
 - Log a message in your chrome dev tools to confirm the file is running.
 */
 
-console.log("hello")
+
+
+
+
 
 /* (2)
 - Select the component with Id of 'container' and save it to a variable
@@ -13,10 +16,10 @@ console.log("hello")
 -console.dir your variable and view it's properties
       - this will always appear as an HTML Object with properties
 */
-const containerElm = document.getElementById('container')
 
-console.log(containerElm)
-console.dir(containerElm)
+
+
+
 
 
 /* (3) 
@@ -26,9 +29,10 @@ console.dir(containerElm)
 (view your results in the elements tab of your chrome dev tools)
 */
 
-const boxContainerEl = document.createElement('div')
-boxContainerEl.className = 'box-container'
-containerElm.appendChild(boxContainerEl)
+
+
+
+
 
 /* (4)
 - using a for loop:
@@ -37,13 +41,11 @@ containerElm.appendChild(boxContainerEl)
     - add a data attribute called 'number' (data-number) equal to the index + 1
        - if index + 1 is greater then 10 set value to 0
 */
-for (let i = 0; i < 10; i++) {
-    const element = document.createElement('div');
-    const number = i + 1 >= 10 ? 0 : i + 1
-    element.className = 'box box' + (i + 1)
-    element.setAttribute('data-number', number)
-    boxContainerEl.appendChild(element)
-}
+
+
+
+
+
 
 /* (5)
 - using the selected box-container element saved above:
@@ -51,10 +53,10 @@ for (let i = 0; i < 10; i++) {
     - iterate over the children and set the internal HTML to the value found in it's data-number attribute
 */
 
-const boxes = boxContainerEl.children
-for (element of boxes) {
-    element.innerHTML = element.dataset['number']
-}
+
+
+
+
 
 /* (6)
 - using the box-container element from above:
@@ -62,22 +64,17 @@ for (element of boxes) {
     - add this div as a sibling before the box-container element
 */
 
-const parent = boxContainerEl.parentElement
-const numberAreaEl = document.createElement('div')
-numberAreaEl.id = 'number-area'
-parent.prepend(numberAreaEl)
+
+
+
+
 
 /* BONUS!!!
 - set an event listener on every box element where clicking to run a function
 - this function should update the inner value of the #number element with the data-number of the element
 */
 
-for (element of boxes) {
-    element.addEventListener('click', updateNumberArea)
-}
 
-function updateNumberArea(e) {
-    numberAreaEl.innerHTML = e.target.dataset['number']
-}
+
 
 
